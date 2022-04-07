cfb02e0f6412226afea805653d8809962b158657
InuGans <102963507+Xenz-11@users.noreply.github.com>
2022-04-07 17:37:41 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+# !/bin/bash/sh
+pkg update && pkg upgrade
+pkg install nano
+pkg install git
+pkg install python
+pip install requests
+git clone https://github.com/Xenzi-XN1/Brutal-Wa
+cd Brutal-Wa
+python Run.py
